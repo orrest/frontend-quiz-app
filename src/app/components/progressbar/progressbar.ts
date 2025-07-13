08dc5a1e4e3e86f05a325cd920e8559805cab608
orrest <zzhxufeng@163.com>
2025-07-13 16:02:40 +0800

    progressbar

---
+++ src/app/components/progressbar/progressbar.ts
@@ -0,0 +1,24 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-progressbar',
+  imports: [],
+  templateUrl: './progressbar.html',
+  styleUrl: './progressbar.css',
+  host: {
+    '[class]': "'flex bg-white p-1 h-4 items-center rounded-full'",
+  },
+})
+export class Progressbar {
+  value = input.required<number, number>({
+    transform: (value: number) => {
+      if (value < 0) {
+        return 0;
+      } else if (value > 100) {
+        return 100;
+      } else {
+        return value;
+      }
+    },
+  });
+}
