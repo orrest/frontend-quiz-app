f04e83a196aa9ad037b395a832b81f8650fcb2d1
orrest <zzhxufeng@163.com>
2025-07-15 10:53:34 +0800

    category route

--- src/app/category-page/category-page.ts
+++ src/app/category-page/category-page.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { DataService } from '../services/data-service';
 import { CategoryItem } from '../components/category-item/category-item';
 import { CategoryItemVm } from '../models/vms/category-item.vm';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-page',
-  imports: [CategoryItem],
+  imports: [CategoryItem, RouterLink],
   templateUrl: './category-page.html',
   styleUrl: './category-page.css',
 })
